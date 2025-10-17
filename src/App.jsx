d0133689d03aa659b3223be3bd2853b0fc173aac
mateus weinert <mateuswhj@gmail.com>
2025-10-17 18:03:21 -0300

    add grid and .map()

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,58 @@
+/* eslint-disable no-unused-vars */
 import './App.css'
 import { Title } from './components/Title'
 import { MultiActionAreaCard } from './components/CardComponent'
 import * as React from 'react';
-
+import { Grid, Paper } from "@mui/material";
 
 function App() {
 
+  const itens = [
+    {
+      name: "card 1",
+      image_url: "x",
+      message: "x",
+      link: "x"
+    },
+    {
+      name: "card 2",
+      image_url: "x",
+      message: "x",
+      link: "x"
+    },
+    {
+      name: "card 3",
+      image_url: "x",
+      message: "x",
+      link: "x"
+    },
+    {
+      name: "card 4",
+      image_url: "x",
+      message: "x",
+      link: "x"
+    },
+    {
+      name: "card 5",
+      image_url: "x",
+      message: "x",
+      link: "x"
+    }
+  ]
 
   return (
     <>
       <div>
         <Title></Title>
-        <MultiActionAreaCard></MultiActionAreaCard>
+        <Grid container spacing={2} justifyContent="center" alignItems="center">
+          {itens.map((user) => (
+            <Grid item>
+              <MultiActionAreaCard></MultiActionAreaCard>
+            </Grid>
+          ))}
 
-        <MultiActionAreaCard></MultiActionAreaCard>
-
-        <MultiActionAreaCard></MultiActionAreaCard>
+        </Grid>
       </div>
-      
     </>
   )
 }
